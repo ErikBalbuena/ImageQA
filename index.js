81a3f2fb94e322c82a85b6bdbc587564a32d11ac
ProteusDexter <morenobello@gmail.com>
2022-05-20 09:38:19 -0500

    mostrar/ocultar intercalar imagenes con cursor

---
+++ index.js
@@ -0,0 +1,12 @@
+function mostrar(input) {
+     let img=document.getElementById("img");
+     if(input.value=="Ocultar")
+     {
+         img.style.visibility="hidden";
+         input.value="Mostrar"
+     }
+     else{
+       img.style.visibility="visible";
+       input.value="Ocultar"
+     }    
+}
