8f985f2c578fc2ee1a74d35563f0b7f81c4c40d8
AdrianoSayeg <104480062+AdrianoSayeg@users.noreply.github.com>
2022-05-23 17:58:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function mostrar(input) {
        input.value="Ocultar"
      }    
 }
+function previewFile() {
+  const preview = document.querySelector('img');
+  const file = document.querySelector('input[type=file]').files[0];
+  const reader = new FileReader();
+
+  reader.addEventListener("load", function () {
+    // convierte la imagen a una cadena en base64
+    preview.src = reader.result;
+  }, false);
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+}
