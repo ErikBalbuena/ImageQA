329bc502f9aeac1ddfe80d23d5dcde692ad7a8cd
ProteusDexter <morenobello@gmail.com>
2022-05-24 19:47:02 -0500

    update de css

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 function mostrar(input) {
-     let img=document.getElementById("img");
-     if(input.value==="Ocultar")
-     {
-         img.style.visibility="hidden";
-         input.value="Mostrar";
-     }
-     else{
-       img.style.visibility="visible";
-       input.value="Ocultar"
-     }    
+  let img=document.getElementById("img");
+  if(input.value==="Ocultar")
+  {
+      img.style.visibility="hidden";
+      input.value="Mostrar";
+  }
+  else{
+    img.style.visibility="visible";
+    input.value="Ocultar"
+  }    
 }
+function previewFile() {
+const preview = document.querySelector('img');
+const file = document.querySelector('input[type=file]').files[0];
+const reader = new FileReader();
+
+reader.addEventListener("load", function () {
+ // convierte la imagen a una cadena en base64
+ preview.src = reader.result;
+}, false);
+if (file) {
+ reader.readAsDataURL(file);
+}
+}
+
